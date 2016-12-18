bed54ede6b2b4e51513782f3327d740302dea2d1
Jan Kuri <jkuri88@gmail.com>
2016-12-18 16:27:09 +0100

    refactor source files

--- src/bootstrap.ts
+++
@@ -1,6 +0,0 @@
-
-import {platformBrowser} from '@angular/platform-browser/index';
-import {AppModuleNgFactory} from './app.module.ngfactory';
-import {Basic} from './basic';
-
-platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
