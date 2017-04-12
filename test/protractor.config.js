5b001fe634133fe09fdc4799104313e0f785f50f
Alex Eagle <alexeagle@google.com>
2017-04-12 14:40:52 -0700

    try more

--- test/protractor.config.js
+++ test/protractor.config.js
@@ -1,15 +1,3 @@
-(function() {
-    var childProcess = require("child_process");
-    var oldSpawn = childProcess.spawn;
-    function mySpawn() {
-        console.log('spawn called');
-        console.log(arguments);
-        var result = oldSpawn.apply(this, arguments);
-        return result;
-    }
-    childProcess.spawn = mySpawn;
-})();
-
 exports.config = {
   specs: [
     '../built/test/*.e2e-spec.js'
