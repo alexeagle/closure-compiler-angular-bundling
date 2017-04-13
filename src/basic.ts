5f1f0dba6fd5b1b551f8ac17e511a6a748afdd81
Torgeir Helgevold <torgeirh@hotmail.com>
2017-04-12 21:56:14 -0400

    add moment

--- src/basic.ts
+++ src/basic.ts
@@ -1,12 +1,13 @@
-
 import {Component, Injectable} from '@angular/core';
+
+import 'moment';
+declare var moment: any;
 
 @Component({
   selector: 'basic',
   templateUrl: './basic.ng.html',
 })
-@Injectable()
 export class Basic {
   ctxProp: string;
-  constructor() { this.ctxProp = 'Hello world!'; }
+  constructor() { this.ctxProp = moment().format('dddd'); }
 }
