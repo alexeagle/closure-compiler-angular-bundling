a1561036891b9623d19e4b5fd160a2b8d7161829
Alex Eagle <alexeagle@google.com>
2017-09-20 08:14:03 -0700

    empty branch

--- src/bootstrap.ts
+++
@@ -1,7 +0,0 @@
-import {platformBrowser} from '@angular/platform-browser';
-import {AppModuleNgFactory} from './app.module.ngfactory';
-import {Basic} from './basic';
-
-import 'rxjs/add/operator/first';
-
-platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
